db909357c5e0f6886fe1acaae3cf4fd4e5e9545b
Daniele <daniele.bedeschi94@gmail.com>
2021-03-15 12:16:11 +0100

    Test Vue Api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,21 @@
 require('./bootstrap');
 
+import axios from 'axios';
 import Vue from 'vue/dist/vue';
 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    restaurants: []
+  },
+  mounted: function() {
+    axios
+    .get('http://127.0.0.1:8000/api/restaurants')
+    .then((response) => {
+      this.restaurants = response.data.results;
+      console.log(this.restaurants);
+    });
   }
+
 })
