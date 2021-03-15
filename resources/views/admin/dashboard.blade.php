3d133e85353a0cf0dc571566fa1579e685c8cb10
Daniele <daniele.bedeschi94@gmail.com>
2021-03-15 10:58:16 +0100

    Commit correzione rotte dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-     </head>
-    <body>
-        <div class="container">
-            <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Piatti</a>
-            <a class="btn btn-secondary" href="">Ordini</a>
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <a class="btn btn-primary" href="{{ route('admin.dishes.index') }}">Piatti</a>
+        <a class="btn btn-secondary" href="">Ordini</a>
+    </div>
+@endsection
