88ee9a741fbcbe9ad49d7af1036a8119b642ea8b
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-03-17 14:03:55 +0100

    footer

---
+++ resources/views/guest/footer.blade.php
@@ -0,0 +1,83 @@
+<div id="footer-top">
+    <div class="mycontainer">
+        <div>
+            <span>Connect with us for update and offers.</span>
+            <div>
+                <form action="">
+                    <input type="email"placeholder="Enter your email">
+                    <button>Subscrive now</button>
+                </form>
+            </div>
+        </div>
+        <div>
+            <select name="" id="">
+                <option value="En">English</option>
+                <option value="It">Italiano</option>
+            </select>
+        </div>
+    </div>
+</div>
+<div id="footer-center">
+    <div class="mycontainer">
+        <div id="left">
+            <h4>Deliveboo</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum dolore non magni modi numquam, sint repudiandae excepturi natus laborum aliquam? Ea nemo hic error aliquid minima iure sapiente, eaque laudantium?</p>
+        </div>
+        <div id="flex-column">
+            <div>
+                <h4>About Deliveboo</h4>
+                <ul>
+                    <li><a href="">About us</a> </li>
+                    <li><a href="">Carrers</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Developers</a></li>
+                    <li><a href="">Mobile apps</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div>
+            <h4>Business</h4>
+                <ul>
+                <li><a href="">Add a restaurant</a></li>
+                <li><a href="">Buniess Order Guidelines</a></li>
+                <li><a href="">Orders</a></li>
+                <li><a href="">Book</a></li>
+                <li><a href="">Trace</a></li>
+                <li><a href="">Advertise</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Partner with us</h4>
+                <ul>
+                    <li><a href="">For restaurant</a></li>
+                    <li><a href="">For drivers</a></li>
+                </ul>
+                <ul class="inline-ul">
+                    <li><i class="fab fa-facebook"></i></li>
+                    <li><i class="fab fa-twitter"></i></li>
+                    <li><i class="fab fa-instagram"></i></li>
+                    <li><i class="fab fa-linkedin-in"></i></li>
+                    <li><i class="fab fa-youtube"></i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="footer-bottom">
+    <div class="mycontainer">
+        <div>
+            <ul class="inline-ul">
+                <li><a href="">Privacy</a></li>
+                <li><a href="">Terms & conditions</a></li>
+                <li><a href="">Sitemap</a></li>
+            </ul>
+        </div>
+        <div>
+            <img src="{{ asset("https://lh3.googleusercontent.com/proxy/j3jZzJN-JPmuiUNvArHuilEZe_5tAQJRASco9vso9MwSrt0N0CQPPT2dJiHm0uW6QDO-xKBvbbrGsaZqPjSFs3LbejZPFGpNokgChZEgkqFXhTrWbeRRxFh-G4hG7aNFCt8hrC7JjQ")}}" alt="">
+        </div>
+    </div>
+</div>
+
+       
+
+
