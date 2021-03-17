3d8571dab25aad1a0a0c50f2f4c531e835905d56
Daniele <daniele.bedeschi94@gmail.com>
2021-03-17 17:29:30 +0100

    fix nav

--- resources/views/guest/header.blade.php
+++ resources/views/guest/header.blade.php
@@ -4,7 +4,7 @@
     <div class="mycontainer">
         <div class="login-header-top">
             <h1 id="logo">DeliveBoo</h1>
-            <button class="logout"><a href="{{ url('/home') }}">Home</a></button>
+           <a  class="logout" href="{{ url('/home') }}">Home</a>
         </div>
     </div>
        
@@ -13,9 +13,9 @@
             <div class="register-header-top">
                 <h1 id="logo">DeliveBoo</h1>
                 <div class="route-in-out">
-                    <button class="login"><a href="{{ route('login') }}">Login</a></button>
+                    <a class="login" href="{{ route('login') }}">Login</a>
                     @if (Route::has('register'))
-                    <button class="register"><a href="{{ route('register') }}">Register</a></button> 
+                        <a class="register" href="{{ route('register') }}">Registrazione</a>
                     @endif
                 </div>
             </div>
