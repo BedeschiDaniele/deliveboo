4f7d0ceb81e696e6257289b9250c8eb78fa594e6
Daniele <daniele.bedeschi94@gmail.com>
2021-03-18 12:46:43 +0100

    cart.js

---
+++ resources/js/cart.js
@@ -0,0 +1,20 @@
+require('./bootstrap');
+
+import axios from 'axios';
+import Vue from 'vue/dist/vue';
+
+var app = new Vue({
+  el: '#cart',
+  data: {
+    cart: [],
+    idCart:[],
+    count:0
+  },
+  methods: {
+    
+  },
+  mounted: function() {
+  
+  }
+
+})
--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.guest.main')
 
 @section('content')
-    <h1>Ristorante {{$restaurant->name}}</h1>
+<div id="cart">
+  <div class="mycontainer">
+    <div class="menu">
+      <h1>Ristorante {{$restaurant->name}}</h1>
+      @foreach ($restaurant->dishes as $dish)
+          <p>{{$dish->name}}</p>
+          <div>
+            <span @click='addToCart({!! json_encode($dish) !!})'>Aggiungi</span>
+          </div>
+      @endforeach
+    </div>
+    <div class="show-cart" v-for='item in cart'>
+      <h3>@{{item.name}}</h3>
+      <span>@{{item.price}}</span>
+    </div>
+    
+  </div>
+</div>
+<script src="{{ asset('js/cart.js') }}"></script>
 @endsection
