be75e4808baccb019e019bc293a58d2d7a6fa83f
adri <adri.kolaj@gmail.com>
2021-03-27 16:52:30 +0100

    fix animazione

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -100,10 +100,10 @@
   {{-- Loading --}}
   <div id="loading">
     <svg width="250" height="250" viewBox="0 0 100 100">
-      <polyline class="line-cornered stroke-still" points="0,0 100,0 100,100" stroke-width="10" fill="none"></polyline>
-      <polyline class="line-cornered stroke-still" points="0,0 0,100 100,100" stroke-width="10" fill="none"></polyline>
-      <polyline class="line-cornered stroke-animation" points="0,0 100,0 100,100" stroke-width="10" fill="none"></polyline>
-      <polyline class="line-cornered stroke-animation" points="0,0 0,100 100,100" stroke-width="10" fill="none"></polyline>
+      <polyline class="line-cornered stroke-still" points="0,0 100,0 100,100" stroke-width="20" fill="none"></polyline>
+      <polyline class="line-cornered stroke-still" points="0,0 0,100 100,100" stroke-width="20" fill="none"></polyline>
+      <polyline class="line-cornered stroke-animation" points="0,0 100,0 100,100" stroke-width="20" fill="none"></polyline>
+      <polyline class="line-cornered stroke-animation" points="0,0 0,100 100,100" stroke-width="20" fill="none"></polyline>
     </svg>
   </div>
 </div>
